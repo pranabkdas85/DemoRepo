1945d3606fe60e1634597a578deb1882d47ce62a
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-17 00:25:04 +0530

    adding module

---
+++ learntestng/src/test/java/com/learntestng/learntestng/AppTest.java
@@ -0,0 +1,11 @@
+package com.learntestng.learntestng;
+
+import org.testng.annotations.Test;
+
+
+public class AppTest {
+ @Test
+ public void sendMail(){
+	 System.out.println("*****************APPTEST**********************");
+ }
+}
---
+++ learntestng/src/test/java/com/learntestng/learntestng/SampleTest.java
@@ -0,0 +1,10 @@
+package com.learntestng.learntestng;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+@Test
+public void SampleTest(){
+System.out.println("*********sampleTest************");
+}
+}
