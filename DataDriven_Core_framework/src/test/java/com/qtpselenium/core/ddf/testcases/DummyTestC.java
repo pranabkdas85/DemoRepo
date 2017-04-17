69a7bd31666532671b340daafb34706e1e89e39f
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-18 00:16:38 +0530

    updated dummy C

--- DataDriven_Core_framework/src/test/java/com/qtpselenium/core/ddf/testcases/DummyTestC.java
+++ DataDriven_Core_framework/src/test/java/com/qtpselenium/core/ddf/testcases/DummyTestC.java
@@ -6,22 +6,19 @@ import com.relevantcodes.extentreports.LogStatus;
 
 import ddfframework.Base.Base;
 
-public class DummyTestC extends Base{
-	
+public class DummyTestC extends Base {
 	@Test
-	public void TestC(){
-		test=rep.startTest("DummyTestC");
-	System.out.println("das");
-	test.log(LogStatus.INFO,"Inside C");
-		
-	test.log(LogStatus.FAIL,"Failed");
-	
-	TakeScreenShot();
-	
-	test.log(LogStatus.INFO,"ScreenShot-->>"+test.addScreenCapture("D:\\test.jpg"));
+	public void TestC() {
+		test = rep.startTest("DummyTestC");
+		System.out.println("das");
+		test.log(LogStatus.INFO, "Inside C");
+		test.log(LogStatus.FAIL, "Failed");
+		TakeScreenShot();
+		test.log(LogStatus.INFO, "ScreenShot-->>" + test.addScreenCapture("D:\\test.jpg"));
 	}
+
 	@AfterMethod
-	public void quit(){
+	public void quit() {
 		rep.endTest(test);
 		rep.flush();
 	}
