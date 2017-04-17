e6b5d045f8f2cf82e43639ade6fe8d671595168a
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-17 15:08:01 +0530

    updated

--- Capgemini/src/practice/Pyramid.java
+++ Capgemini/src/practice/Pyramid.java
@@ -3,21 +3,17 @@ package practice;
 public class Pyramid {
 
 	public static void main(String[] args) {
-		int Height = 3;
-		int blank = 0;
-		for (int i = 0; i < Height; i++) {
-			blank = Height / (2+i);
-			for (int j = 0; j < blank; j++) {
+		int rows=9;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows - i; j++) {
 				System.out.print(" ");
 			}
-
-			System.out.print("*");
-			for (int j = 0; j < blank; j++) {
-				System.out.print(" ");
+			for (int k = 0; k <= i; k++) {
+				System.out.print("* ");
 			}
-			System.out.println("");
+			System.out.println();
 		}
-
 	}
-
 }
+
+
--- Capgemini/src/practice/Pyramidshape.java
+++ Capgemini/src/practice/Pyramidshape.java
@@ -1,5 +1,18 @@
 package practice;
 
 public class Pyramidshape {
+	public static void main(String args[]) {
 
+		int rows = 9;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
 }
