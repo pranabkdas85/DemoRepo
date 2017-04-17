6e31cfb9d4a4f5b2249b15d56b3dd2da523e860e
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-18 01:00:39 +0530

    corrected the fifthtype of refresh

--- Capgemini/src/Q10.java
+++ Capgemini/src/Q10.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class Q10 {
 	public static void main(String args[]) {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
 
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		FirefoxOptions options = new FirefoxOptions();
@@ -23,18 +23,23 @@ public class Q10 {
 
 		// 1st type
 		driver.navigate().refresh();
+		System.out.println("completed the first type");
 
 		// 2nd Type
 		driver.findElement(By.id("usernameId")).sendKeys(Keys.F5);
+		System.out.println("completed the second type");
 
 		// 3rd Type
 		driver.get(driver.getCurrentUrl());
+		System.out.println("completed the third type");
 
 		// 4th Type
 		driver.navigate().to(driver.getCurrentUrl());
+		System.out.println("completed the forth type");
 
 		// 5th type
-		driver.findElement(By.name("s")).sendKeys("\uE035");
+		driver.findElement(By.name("j_captcha")).sendKeys("\uE035");
+		System.out.println("completed the fifth type");
 
 	}
 }
