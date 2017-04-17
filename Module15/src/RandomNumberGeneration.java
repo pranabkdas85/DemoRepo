25702baa24468a1c0182cb6917cf300228632eec
Ram Sharma <ramsharma.india001@gmail.com>
2017-04-17 06:49:58 +0530

    Updated classpath as per requirement of various projects

---
+++ Module15/src/RandomNumberGeneration.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RandomNumberGeneration {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt()); 
+
+	}
+
+}
--- Module4/src/exercise1.java
+++ Module4/src/exercise1.java
@@ -1,39 +1,42 @@
 
 public class exercise1 {
 
-		public static void main(String[] a){
-		A a1 = new B();
+	public static void main(String[] a) {
+		exercise1 ex = new exercise1();
+		A a1 = ex.new B();
 		a1.meth1();
 		a1.meth2();
 		a1.meth3();
-		//a1.meth4();
-		}
+		// a1.meth4();
+	}
 
-
-		interface A{
+	interface A {
 		public void meth1();
+
 		public void meth2();
+
 		public void meth3();
-		}
+	}
 
-
-
-		class B implements A{
+	class B implements A {
 
 		@Override
 		public void meth1() {
-		System.out.println("meth1");
+			System.out.println("meth1");
 		}
+
 		@Override
 		public void meth2() {
-		System.out.println("meth2");
+			System.out.println("meth2");
 		}
+
 		@Override
 		public void meth3() {
-		System.out.println("meth3");
+			System.out.println("meth3");
 		}
+
 		public void meth4() {
-		System.out.println("meth4");
+			System.out.println("meth4");
 		}
-		}
+	}
 }
