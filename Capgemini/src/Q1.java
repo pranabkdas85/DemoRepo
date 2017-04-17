e67140fead5ac8a24355f06f14a154a30c3ea677
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-18 00:48:13 +0530

    commit and save

--- Capgemini/src/Q1.java
+++ Capgemini/src/Q1.java
@@ -8,24 +8,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Q1 {
-public static void main(String args[]){
-	String browser= "Chrome";
-	WebDriver driver=null;
-	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-	System.setProperty("webdriver.ie.driver","C:\\IEDriverServer.exe");
-	if (browser.equals("Chrome")){
-		ChromeOptions options = new ChromeOptions();
-		Map<String, Object> chromePrefs = new HashMap<String, Object>();
-		chromePrefs.put("credentials_enable_service", false);
-		options.setExperimentalOption("prefs", chromePrefs);
-		options.addArguments("disable-infobars");
-		driver = new ChromeDriver(options);
+	public static void main(String args[]) {
+		String browser = "Chrome";
+		WebDriver driver = null;
+
+		System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
+		if (browser.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			Map<String, Object> chromePrefs = new HashMap<String, Object>();
+			chromePrefs.put("credentials_enable_service", false);
+			options.setExperimentalOption("prefs", chromePrefs);
+			options.addArguments("disable-infobars");
+			driver = new ChromeDriver(options);
+		} else if (browser.equals("IE")) {
+			driver = new InternetExplorerDriver();
+		}
+
+		driver.get("https://www.irctc.co.in/eticketing/loginHome.jsf");
+
 	}
-	else if (browser.equals("IE")){
-		driver= new InternetExplorerDriver();
-	}
-	
-	driver.get("https://www.irctc.co.in/eticketing/loginHome.jsf");
-	
-}
 }
