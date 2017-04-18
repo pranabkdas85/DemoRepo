7cc0cab33bab7cc8bf37ad3b7aae02c75b564b6a
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-19 03:38:03 +0530

    new prog

--- Capgemini/src/practice/Swaptwonumberswithoutusingthirdvariable.java
+++ Capgemini/src/practice/Swaptwonumberswithoutusingthirdvariable.java
@@ -1,5 +1,21 @@
 package practice;
 
 public class Swaptwonumberswithoutusingthirdvariable {
+	
+	public static void main(String args[]){
+	
+	int x = 10, y = 20;
+	 
 
-}
+	  x = x + y;  // x = 30
+	  y = x - y;  // y =10
+	  x = x - y;  // x =20
+	  
+	  
+	  
+	  System.out.println("x:"+ x);
+	  System.out.println("y:"+ y);
+	}
+	}
+
+
