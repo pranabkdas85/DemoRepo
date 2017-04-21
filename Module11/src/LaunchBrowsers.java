b16a16fe30cd31644c5b7d7e48290cdd3b65d949
pranabkdas85 <pranabk.das85@gmail.com>
2017-04-21 14:55:27 +0530

    updated

--- Module11/src/LaunchBrowsers.java
+++ Module11/src/LaunchBrowsers.java
@@ -5,44 +5,44 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 public class LaunchBrowsers {
 
 	String Browser = "Chrome";
-	  
-	@Test( dataProvider= "getdata")
-	public void launch(String Browser, String username){
-		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-		System.setProperty("webdriver.ie.driver","C:\\IEDriverServer.exe");
-		
-		WebDriver driver= null;
-		if(Browser.equals("Morzilla"))
+
+	@Test(dataProvider = "getdata")
+	public void launch(String Browser, String username) {
+
+		WebDriver driver = null;
+		if (Browser.equals("Mozilla")) {
+			System.setProperty("webdriver.gecko.driver", System.getenv("FIREFOXDRIVER_HOME"));
 			driver = new FirefoxDriver();
-		else if(Browser.equals("Chrome"))
-			driver= new ChromeDriver();
-		else if (Browser.equals("Internet Explorer"))
-			driver= new InternetExplorerDriver();
+		} else if (Browser.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_HOME"));
+			driver = new ChromeDriver();
+		} else if (Browser.equals("Internet Explorer")) {
+			System.setProperty("webdriver.ie.driver", System.getenv("INTERNETEXPLORERDRIVER_HOME"));
+			driver = new InternetExplorerDriver();
+		}
 		driver.get("http://gmail.com");
 		driver.quit();
-			
-			}
-	@DataProvider
-	public Object[][] getdata()
-	{
-		Object[][] data=new Object[3][2];
-		// 3 iterations and 2 number of data
-		data[0][0]= "Chrome";
-		data[0][1]= "user1";
-		
-		data[1][0]= "Morzilla";
-		data[1][1]= "user2";
-		
-		data[2][0]= "Internet Explorer";
-		data[2][1]= "user3";
-		
-		return data;		
-		
+
 	}
-	
+
+	@DataProvider
+	public Object[][] getdata() {
+		Object[][] data = new Object[3][2];
+		// 3 iterations and 2 number of data
+		data[0][0] = "Chrome";
+		data[0][1] = "user1";
+
+		data[1][0] = "Morzilla";
+		data[1][1] = "user2";
+
+		data[2][0] = "Internet Explorer";
+		data[2][1] = "user3";
+
+		return data;
+
+	}
+
 }
