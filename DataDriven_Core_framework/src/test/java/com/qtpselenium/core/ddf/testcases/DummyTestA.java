0a0a966898aa0c595bbfbf6ddb64329a347c81f1
Ram Sharma <ramsharma.india001@gmail.com>
2017-05-01 18:17:50 +0530

    Added functionality to display skip status with testcasename

--- DataDriven_Core_framework/src/test/java/com/qtpselenium/core/ddf/testcases/DummyTestA.java
+++ DataDriven_Core_framework/src/test/java/com/qtpselenium/core/ddf/testcases/DummyTestA.java
@@ -27,8 +27,8 @@ public class DummyTestA extends Base {
 		test = rep.startTest("DummyTestA:TestA1");
 		test.log(LogStatus.INFO, data.toString());
 		if (!DataUtil.isRuunnable(testCaseName, xls) || data.get("Runmode").toUpperCase().equals("N")) {
-			test.log(LogStatus.SKIP, "The  test Case is Skipped as Run Mode is N");
-			throw new SkipException("The  test Case is Skipped as Run Mode is N");
+			test.log(LogStatus.SKIP, testCaseName+": The  test Case is Skipped as Run Mode is N");
+			throw new SkipException(testCaseName+": The  test Case is Skipped as Run Mode is N");
 		}
 		// Assert.fail();
 		test.log(LogStatus.INFO, "Inside A1");
@@ -41,8 +41,8 @@ public class DummyTestA extends Base {
 		test = rep.startTest("DummyTestA:TestA2");
 		test.log(LogStatus.INFO, data.toString());
 		if (!DataUtil.isRuunnable(testCaseName, xls) || data.get("Runmode").equals("N")) {
-			test.log(LogStatus.SKIP, "The  test Case is Skipped as Run Mode is N");
-			throw new SkipException("The  test Case is Skipped as Run Mode is N");
+			test.log(LogStatus.SKIP, testCaseName+": The  test Case is Skipped as Run Mode is N");
+			throw new SkipException(testCaseName+": The  test Case is Skipped as Run Mode is N");
 		}
 		test.log(LogStatus.INFO, "Inside A2");
 		ReportPass("DummyTestA:TestA2 Passed ");
@@ -53,8 +53,8 @@ public class DummyTestA extends Base {
 		testCaseName = "TestA3";
 		test = rep.startTest("DummyTestA:TestA3");
 		if (!DataUtil.isRuunnable(testCaseName, xls) || data.get("Runmode").equals("N")) {
-			test.log(LogStatus.SKIP, "The  test Case is Skipped as Run Mode is N");
-			throw new SkipException("The  test Case is Skipped as Run Mode is N");
+			test.log(LogStatus.SKIP, testCaseName+": The  test Case is Skipped as Run Mode is N");
+			throw new SkipException(testCaseName+": The  test Case is Skipped as Run Mode is N");
 		}
 		test.log(LogStatus.INFO, data.toString());
 		test.log(LogStatus.INFO, "Inside A3");
