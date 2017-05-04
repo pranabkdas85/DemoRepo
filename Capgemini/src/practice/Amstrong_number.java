9f1a2bdab10b4174a395cf8f168c60c6ab47ffa0
pranabkdas85 <pranabk.das85@gmail.com>
2017-05-04 10:42:58 +0530

    Update Amstrong_number.java

--- Capgemini/src/practice/Amstrong_number.java
+++ Capgemini/src/practice/Amstrong_number.java
@@ -18,22 +18,6 @@ public class Amstrong_number {
 			sum = 0;
 		}
 
-	/*	int onedigit, twodigit, thirddigit, fourdigit, rem = 0;
-		double number;
-		int ip_number = 407;
-		fourdigit = ip_number / 1000;
-		rem = ip_number % 1000;
-		thirddigit = rem / 100;
-		rem = ip_number % 100;
-		twodigit = rem / 10;
-		rem = ip_number % 10;
-		onedigit = rem;
-		number = Math.pow(fourdigit, 3) + Math.pow(thirddigit, 3) + Math.pow(twodigit, 3) + Math.pow(onedigit, 3);
-		if (ip_number == number)
-			System.out.println(ip_number + ": is a Amstrong number");
-		else
-			System.out.println(ip_number + ": is not a Amstrong number");*/
-
 	}
 
 }
