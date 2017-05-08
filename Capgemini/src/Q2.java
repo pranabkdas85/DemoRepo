b7133e965c4982285be2fce9fd69020ad572479d
Ram Sharma <ramsharma.india001@gmail.com>
2017-05-08 09:35:20 +0530

    Capegemini/Q2: Added functionality for user to select date

--- Capgemini/src/Q2.java
+++ Capgemini/src/Q2.java
@@ -1,37 +1,140 @@
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Q2 {
 
+	static WebDriver driver;
+	
 	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
-
+		//System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+		/*System.setProperty("webdriver.gecko.driver", "D:/Coding/jars/geckodriver-v0.14.0-win32/geckodriver.exe");
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		FirefoxOptions options = new FirefoxOptions();
 		options.setLogLevel(Level.SEVERE);
 		capabilities.setCapability("moz:firefoxOptions", options);
-		WebDriver driver = new FirefoxDriver(capabilities);
+		WebDriver driver = new FirefoxDriver(capabilities);*/
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Coding/jars/chromedriver_win32_2.29/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		Map<String, Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("credentials_enable_service", false);
+		options.setExperimentalOption("prefs", chromePrefs);
+		options.addArguments("disable-infobars");
+		driver = new ChromeDriver(options);
+		
 		driver.manage().window().maximize();
+		
+		String startDate = "08/06/2021"; // 8th June 2021
+		
+		String endDate = "16/06/2021"; // 16th June 2021
 
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 		driver.get("http://www.gingerhotels.com/");
 		driver.findElement(By.xpath("//div[@class='form-group col-md-12']/select")).sendKeys("Agartala");
-		driver.findElement(By.id("checkin")).click();
-		driver.findElement(By.xpath("html/body/div[2]/div[1]/table/tbody/tr[5]/td[7]")).click();
-		driver.findElement(By.xpath("html/body/div[3]/div[1]/table/tbody/tr[6]/td[1]")).click();
+		
+		//Setting checkin date
+		WebElement checkInEle = driver.findElement(By.id("checkin"));
+		setDate(startDate, checkInEle);
+		
+		//Setting checkout date
+		WebElement checkOutEle = driver.findElement(By.id("checkout"));
+		setDate(endDate, checkOutEle);
+		
 		WebElement room = driver.findElement(By.id("rooms"));
-		List<WebElement> list_room = room.findElements(By.tagName("option"));
-		System.out.println(list_room.size());
 		room.sendKeys("2");
 
+	}
+	
+	public static Calendar getFormattedDate(String dateInString){
+		Date formattedDate = null;
+		Date dateNow = null;
+		SimpleDateFormat newdateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			formattedDate = newdateFormat.parse(dateInString);
+			dateNow  = newdateFormat.parse(newdateFormat.format(new Date()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		Calendar fomattedCalc = Calendar.getInstance();
+		fomattedCalc.setTime(formattedDate);
+		Calendar calNow = Calendar.getInstance();
+		calNow.setTime(dateNow);
+		
+		if(fomattedCalc.before(calNow))
+			return null;
+			
+		return fomattedCalc;
+	}
+		
+	public static void setDate(String dateInString,WebElement dateBox){
+		Calendar cal =  getFormattedDate(dateInString);
+		if(null==cal){
+			System.out.println("Please provide current or future date");
+			Assert.fail();
+			return;
+		}
+		if(dateBox.getAttribute("id").equals("checkin"))
+			dateBox.click();
+		
+		String year = String.valueOf(cal.get(Calendar.YEAR));
+		String month = new SimpleDateFormat("MMM").format(cal.getTime());
+		String date = String.valueOf(cal.get(Calendar.DATE));
+		
+		WebElement datePicker = driver.findElement(By.xpath("//div[@class='datepicker dropdown-menu' and not(contains(@style,'none'))]"));
+		if(datePicker.findElement(By.xpath("./div[contains(@class,'-days')]//th[@class='switch']")).getText().contains(year)){
+			//When we are booking in the same year
+			if(datePicker.findElement(By.xpath("./div[contains(@class,'-days')]//th[@class='switch']")).getText().contains(month)){
+				//When we are booking in the same month
+				datePicker.findElement(By.xpath(".//td[not(contains(@class,'old')) or not(contains(@class,'disabled'))][text()='"+date+"']")).click();
+			}else{
+				//Selecting the target month and date
+				datePicker.findElement(By.xpath("./div[contains(@class,'-days')]//th[@class='switch']")).click();
+				datePicker.findElement(By.xpath(".//span[contains(@class,'month')][text()='"+month+"']")).click();
+				dateBox.click();
+				datePicker.findElement(By.xpath(".//td[not(contains(@class,'old')) or not(contains(@class,'disabled'))][text()='"+date+"']")).click();
+			}
+		}else{
+			//When we are not booking in the same year
+			datePicker.findElement(By.xpath("./div[contains(@class,'-days')]//th[@class='switch']")).click();
+			datePicker.findElement(By.xpath("./div[contains(@class,'-month')]//th[@class='switch']")).click();
+			
+			//Searching for correct year to be found
+			boolean isYearNotInRange = false;
+			do{
+				String yearRange = datePicker.findElement(By.xpath("./div[contains(@class,'-years')]//th[@class='switch']")).getText();
+				isYearNotInRange = Integer.parseInt(yearRange.substring(5, 9))<Integer.parseInt(year);
+				if(isYearNotInRange)
+					datePicker.findElement(By.xpath("./div[contains(@class,'-years')]//th[@class='next']")).click();
+			}while(isYearNotInRange);
+			
+			//Selecting correct year
+			datePicker.findElement(By.xpath(".//span[contains(@class,'year')][text()='"+year+"']")).click();
+			dateBox.click();
+			
+			//Searching for target month and date
+			if(datePicker.findElement(By.xpath("./div[contains(@class,'-days')]//th[@class='switch']")).getText().contains(month)){
+				datePicker.findElement(By.xpath(".//td[not(contains(@class,'old')) or not(contains(@class,'disabled'))][text()='"+date+"']")).click();
+			}else{
+				datePicker.findElement(By.xpath("./div[contains(@class,'-days')]//th[@class='switch']")).click();
+				datePicker.findElement(By.xpath(".//span[contains(@class,'month')][text()='"+month+"']")).click();
+				dateBox.click();
+				datePicker.findElement(By.xpath(".//td[not(contains(@class,'old')) or not(contains(@class,'disabled'))][text()='"+date+"']")).click();
+			}
+		}
+		
 	}
 }
