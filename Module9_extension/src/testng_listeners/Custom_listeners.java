8ec48da0a47de2b84439ecddfaa3cf9e290d91d2
pranabkdas85 <pranabk.das85@gmail.com>
2017-05-11 01:04:43 +0530

    updated

--- Module9_extension/src/testng_listeners/Custom_listeners.java
+++ Module9_extension/src/testng_listeners/Custom_listeners.java
@@ -9,8 +9,7 @@ import org.testng.Reporter;
 import org.testng.TestListenerAdapter;
 import org.testng.internal.Utils;
 
-public class Custom_listeners extends TestListenerAdapter implements
-		IInvokedMethodListener {
+public class Custom_listeners extends TestListenerAdapter implements IInvokedMethodListener {
 
 	// override
 	public void onTestFailure(ITestResult result) {
@@ -28,8 +27,7 @@ public class Custom_listeners extends TestListenerAdapter implements
 	public void afterInvocation(IInvokedMethod method, ITestResult result) {
 		Reporter.setCurrentTestResult(result);
 		if (method.isTestMethod()) {
-			List<Throwable> verificationFailures = ErrorUtil
-					.getVerificationFailures();
+			List<Throwable> verificationFailures = ErrorUtil.getVerificationFailures();
 			// if there are verification failures...
 			if (verificationFailures.size() != 0) {
 				// set the test to failed
@@ -46,19 +44,16 @@ public class Custom_listeners extends TestListenerAdapter implements
 				} else {
 					// create a failure message with all failures and stack
 					// traces (except last failure)
-					StringBuffer failureMessage = new StringBuffer(
-							"Multiple failures (").append(size).append("):nn");
+					StringBuffer failureMessage = new StringBuffer("Multiple failures (").append(size).append("):nn");
 					for (int i = 0; i < size - 1; i++) {
-						failureMessage.append("Failure ").append(i + 1)
-								.append(" of ").append(size).append(":n");
+						failureMessage.append("Failure ").append(i + 1).append(" of ").append(size).append(":n");
 						Throwable t = verificationFailures.get(i);
 						String fullStackTrace = Utils.stackTrace(t, false)[1];
 						failureMessage.append(fullStackTrace).append("nn");
 					}
 					// final failure
 					Throwable last = verificationFailures.get(size - 1);
-					failureMessage.append("Failure ").append(size)
-							.append(" of ").append(size).append(":n");
+					failureMessage.append("Failure ").append(size).append(" of ").append(size).append(":n");
 					failureMessage.append(last.toString());
 					// set merged throwable
 					Throwable merged = new Throwable(failureMessage.toString());
--- Module9_extension/src/testng_listeners/LoginTest.java
+++ Module9_extension/src/testng_listeners/LoginTest.java
@@ -12,7 +12,21 @@ public class LoginTest {
 			Assert.assertEquals("A", "B");
 
 		} catch (Throwable t) {
-			System.out.println("error");
+			System.out.println("error1");
+			ErrorUtil.addVerificationFailure(t);
+		}
+		try {
+			Assert.assertEquals("A", "C");
+
+		} catch (Throwable t) {
+			System.out.println("error2");
+			ErrorUtil.addVerificationFailure(t);
+		}
+		try {
+			Assert.assertEquals("A", "D");
+
+		} catch (Throwable t) {
+			System.out.println("error3");
 			ErrorUtil.addVerificationFailure(t);
 		}
 		System.out.println("ends");
