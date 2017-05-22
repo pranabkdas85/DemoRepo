f365e30d416227ecad92b06bc695a45cc5f2b958
Pranab Das <Pranab Das>
2017-05-22 17:18:04 +0530

    updated

--- com.ddf/src/test/java/Salesforce/com/ddf/Contacts.java
+++ com.ddf/src/test/java/Salesforce/com/ddf/Contacts.java
@@ -1,8 +1,11 @@
 package Salesforce.com.ddf;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Hashtable;
 
+import org.openqa.selenium.support.ui.Select;
 import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,6 +13,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Iterator;
 import com.relevantcodes.extentreports.LogStatus;
 
 public class Contacts extends base.Base {
@@ -17,14 +21,65 @@ public class Contacts extends base.Base {
 
 	@Test(dataProvider = "getData")
 	public void Contacts_dropdown(Hashtable<String, String> data) throws IOException {
-		testCaseName = "Contacts";
+		testCaseName = "Contacts_dropdown";
 		test = rep.startTest("Contacts_dropdown");
 		test.log(LogStatus.INFO, data.toString());
+
 		if (!DataUtil.isRuunnable(testCaseName, xls) || data.get("Runmode").toUpperCase().equals("N")) {
 			test.log(LogStatus.SKIP, testCaseName + ": The  test Case is Skipped as Run Mode is N");
 			throw new SkipException(testCaseName + ": The  test Case is Skipped as Run Mode is N");
 		}
+
 		test.log(LogStatus.INFO, "Starting the Test");
+		OpenBrowser("Chrome");
+		Navigate("appurl");
+		userlogin(Prop.getProperty("uderid"), Prop.getProperty("password"));
+		// click on contact tab
+		Click("contacts_xpath");
+		String[] ActualLOV = getlovs("contactsdropdown_xpath");
+		String[] expectedLOV = data.get("Contacts_LOV").split(",");
+
+		if (!Arrays.equals(ActualLOV, expectedLOV)) {
+			test.log(LogStatus.INFO, "The excepted List of values are :" + Arrays.deepToString(expectedLOV));
+			ReportFail("There is a mismatch in the number of drop down in the contact");
+		} else {
+			ReportPass("The List of values matched for the contacts");
+		}
+
+		driver.close();
+	}
+
+	@Test(dataProvider = "getData")
+	public void createcontacts(Hashtable<String, String> data) throws IOException {
+		testCaseName = "createcontacts";
+		test = rep.startTest("createcontacts");
+		test.log(LogStatus.INFO, data.toString());
+
+		if (!DataUtil.isRuunnable(testCaseName, xls) || data.get("Runmode").toUpperCase().equals("N")) {
+			test.log(LogStatus.SKIP, testCaseName + ": The  test Case is Skipped as Run Mode is N");
+			throw new SkipException(testCaseName + ": The  test Case is Skipped as Run Mode is N");
+		}
+
+		test.log(LogStatus.INFO, "Starting the Test");
+		OpenBrowser("Chrome");
+		Navigate("appurl");
+		userlogin(Prop.getProperty("uderid"), Prop.getProperty("password"));
+		Click("contacts_xpath");
+		Click("contacts_newButton_xpath");
+		String[] fnlov = getlovs("FirstNameLOV_id");
+		String[] expectedfnlov = data.get("fnlov").split(",");
+		test.log(LogStatus.INFO, ("The expected List of Values are :" + Arrays.toString(expectedfnlov)));
+		if (!Arrays.equals(fnlov, expectedfnlov))
+			ReportFail("There is a mismatch in the List of values under Contacts>>New Contacts>>Firstname ");
+		else
+			ReportPass("The List of values under Contacts>>New Contacts>>Firstname matched");
+		Select s = new Select(getelement("FirstNameLOV_id"));
+		s.selectByVisibleText("Mr.");
+		Type("FirstName_id", "Pranab");
+		Type("Middlename_id", "Kumar");
+		Type("Lastname_xpath", "Das");
+		Click("openAccount_xpath");
+		selectvalueinpopupwindow("accountpopup_xpath");
 
 	}
 
@@ -47,10 +102,11 @@ public class Contacts extends base.Base {
 	}
 
 	@DataProvider
-	public Object[][] getData() {
+	public Object[][] getData(Method m) {
 		init();
-		Xls_Reader xls = new Xls_Reader(System.getProperty("user.dir") + "\\Data.xlsx");
-		return DataUtil.getTestData(xls, "TestC");
+		xls = new Xls_Reader(System.getProperty("user.dir") + "\\Data.xlsx");
+		return DataUtil.getTestData(xls, m.getName());
+
 	}
 
 }
--- com.ddf/src/test/java/base/Base.java
+++ com.ddf/src/test/java/base/Base.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -25,9 +28,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Set;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
@@ -51,18 +56,20 @@ public class Base {
 				// FileInputStream fs = new
 				// FileInputStream(System.getProperty("user.dir") +
 				// ("\\src\\test\\resources\\ProjectConfig.Properties"));
-				InputStream fs = new FileInputStream(System.getProperty("user.dir") +"\\src\\main\\java\\src\\test\\resources\\ProjectConfig.Properties");
+				InputStream fs = new FileInputStream(System.getProperty("user.dir")
+						+ "\\src\\main\\java\\src\\test\\resources\\ProjectConfig.Properties");
 				Prop.load(fs);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
+	// ******************generic Functions******************************
 
 	public void OpenBrowser(String Browser) throws IOException {
 		DesiredCapabilities capabilities = null;
 		if (Browser.equals("Mozilla")) {
-			System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
 			capabilities = DesiredCapabilities.firefox();
 			FirefoxOptions options = new FirefoxOptions();
 			options.setLogLevel(Level.SEVERE);
@@ -70,7 +77,7 @@ public class Base {
 			driver = new FirefoxDriver(capabilities);
 
 		} else if (Browser.equals("Chrome")) {
-			System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 			ChromeOptions options = new ChromeOptions();
 			Map<String, Object> chromePrefs = new HashMap<String, Object>();
 			chromePrefs.put("credentials_enable_service", false);
@@ -78,7 +85,7 @@ public class Base {
 			options.addArguments("disable-infobars");
 			driver = new ChromeDriver(options);
 		} else if (Browser.equals("Internet Explorer")) {
-			System.setProperty("webdriver.ie.driver","C:\\IEDriverServer.exe");
+			System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
 			capabilities = DesiredCapabilities.internetExplorer();
 			capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 			driver = new InternetExplorerDriver(capabilities);
@@ -91,6 +98,7 @@ public class Base {
 
 	// Responsible to find element in the Page
 	public WebElement getelement(String locator_Key) {
+		test.log(LogStatus.INFO, "Inside the getelement function");
 		WebElement we = null;
 		try {
 			if (locator_Key.endsWith("_id"))
@@ -116,18 +124,66 @@ public class Base {
 	}
 
 	public void Click(String locator_Key) {
+		test.log(LogStatus.INFO, "clicking on" + locator_Key);
 		getelement(locator_Key).click();
 	}
 
 	public void Type(String locator_Key, String data) {
 		getelement(locator_Key).sendKeys(data);
 	}
-	//**********APP Functions*********************
-	public void userlogin(String userid,String password)
-	{
-		test.log(LogStatus.INFO, "login by id: " +userid +" and password: " + password);
+
+	public String[] getlovs(String locator_key) {
+		test.log(LogStatus.INFO, "Inside the getlov function to get the list of values");
+		ArrayList<String> arl = new ArrayList<String>();
+		Select s = new Select(getelement(locator_key));
+		List<WebElement> we = s.getOptions();
+		for (int i = 0; i < we.size(); i++) {
+			arl.add(we.get(i).getText());
+		}
+		String[] str = new String[arl.size()];
+		arl.toArray(str);
+		test.log(LogStatus.INFO, "The Actual List of values are :" + Arrays.toString(str));
+		return str;
+	}
+
+	public void closepopupwindows() {
+		java.util.Set<String> windowids = driver.getWindowHandles();
+		Iterator<String> it = windowids.iterator();
+		String Mainwindow = it.next();
+		while (it.hasNext()) {
+			String popup_window = it.next();
+			driver.switchTo().window(popup_window);
+			driver.close();
+		}
+		driver.switchTo().window(Mainwindow);
+	}
+
+	public void selectvalueinpopupwindow(String locator_key) {
+		java.util.Set<String> windowsid = driver.getWindowHandles();
+		System.out.println("the number of windows are " + windowsid.size());
+		java.util.Iterator<String> it = windowsid.iterator();
+		String Mainwindows = it.next();
+		String popup_window = null;
+		while (it.hasNext()) {
+			popup_window = it.next();
+			driver.switchTo().window(popup_window);
+			test.log(LogStatus.INFO ,"Window switched");
+			if(Iselementpresent(locator_key))
+			{
+				test.log(LogStatus.INFO,"The element is found");
+				Click(locator_key);
+				break;
+			}
+		}
+		driver.switchTo().defaultContent();
+		
+	}
+
+	// **********APP Functions*********************
+	public void userlogin(String userid, String password) {
+		test.log(LogStatus.INFO, "login by id: " + userid + " and password: " + password);
 		Type("Loginid_xpath", userid);
-		Type("password_xpath",password);
+		Type("password_xpath", password);
 		Click("Loginbutton_xpath");
 	}
 
