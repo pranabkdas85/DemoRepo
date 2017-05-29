6e4c5e2ae450b599ca35a27396857fe5d36cfbb1
Pranab Das <Pranab Das>
2017-05-29 15:57:29 +0530

    updated

--- com.ddf/src/main/java/Salesforce/com/ddf/CustomListeners.java
+++ com.ddf/src/main/java/Salesforce/com/ddf/CustomListeners.java
@@ -6,7 +6,6 @@ import org.testng.TestListenerAdapter;
 public class CustomListeners extends TestListenerAdapter {
 
 	public void onTestSuccess(ITestResult tr) {
-		System.out.println("The test Case is Passed");
 
 	}
 
